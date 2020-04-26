c969cf0094ccefc860e0633e2c9a626bb1237e33
Ahsun Ansari <63501165+designer-web@users.noreply.github.com>
2020-04-26 18:03:48 +0530

    Update script.js

--- assignments/assignment3/js/script.js
+++ assignments/assignment3/js/script.js
@@ -28,7 +28,7 @@
 
 (function () {
 
-		var names = ["Ahsun", "John", "Jen", "Suhani", "Paul", "Frank", "Johnny", "Paula", "Jim", "Laura"];
+		var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 		for (var i = 0; i < names.length; i++) {
 			var firstLetter = names[i].charAt(0).toLowerCase();
 			if (firstLetter === 'j') {
